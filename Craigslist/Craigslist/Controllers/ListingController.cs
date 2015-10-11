331a621ac3e51872f1b2dbf8baf21032a035da3e
Alex <AlexTheProgrammerMail@gmail.com>
2015-10-10 20:00:11 -0700

    Dummy Listings view created, JS libraries pulled,

---
+++ Craigslist/Craigslist/Controllers/ListingController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Craigslist.Controllers
+{
+    public class ListingController : Controller
+    {
+	    public ViewResult List()
+	    {
+		    return View();
+	    }
+    }
+}
