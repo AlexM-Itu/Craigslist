983378267fa822bc0847e348696910c62a1646ee
Alex <AlexTheProgrammerMail@gmail.com>
2015-10-10 20:45:49 -0700

    LisingsManager created with a method to add a listing.

---
+++ Craigslist/Craigslist.Business/ListingsManager.cs
@@ -0,0 +1,17 @@
+﻿using Craigslist.Domain;
+using Craigslist.Domain.Entities;
+
+namespace Craigslist.Business
+{
+	public class ListingsManager
+	{
+		public void PublishListing(Listing listing)
+		{
+			using (var domain = new CraigslistDomain())
+			{
+				domain.Listings.Add(listing);
+				domain.SaveChanges();
+			}
+		}
+	}
+}
