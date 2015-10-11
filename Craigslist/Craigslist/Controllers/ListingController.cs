31702335cb8a7de3989f44437fd54ef0d0c611ab
Alex <AlexTheProgrammerMail@gmail.com>
2015-10-10 22:20:24 -0700

    Controller logic to publish a listing

--- Craigslist/Craigslist/Controllers/ListingController.cs
+++ Craigslist/Craigslist/Controllers/ListingController.cs
@@ -1,16 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using Craigslist.Business;
+using Craigslist.Domain.Entities;
 
 namespace Craigslist.Controllers
 {
     public class ListingController : Controller
     {
+		private readonly ListingsManager manager = new ListingsManager();
+
 	    public ViewResult List()
 	    {
 		    return View();
+	    }
+
+	    public ViewResult Publish()
+	    {
+		    return View();
+	    }
+
+		[HttpPost]
+	    public ActionResult Publish(Listing listing)
+	    {
+		    if (ModelState.IsValid)
+		    {
+			    manager.PublishListing(listing);
+				return new RedirectToRouteResult(new RouteValueDictionary());
+		    }
+
+			return View();
 	    }
     }
 }
