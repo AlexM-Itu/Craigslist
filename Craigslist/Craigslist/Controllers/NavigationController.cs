e8b3d566d58321fc5fd99e7d1b77286fe5b09860
Alex <AlexTheProgrammerMail@gmail.com>
2015-10-11 16:09:20 -0700

    Navigation topbar bare

---
+++ Craigslist/Craigslist/Controllers/NavigationController.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Craigslist.Business;
+
+namespace Craigslist.Controllers
+{
+    public class NavigationController : Controller
+    {
+	    private readonly LookupManager lookupManager = new LookupManager();
+
+		public ActionResult Index()
+        {
+			var allCategories = lookupManager.GetAllCategories().ToList();
+			var categories = allCategories.Select(catetory => new SelectListItem
+			{
+				Value = catetory.Id.ToString(),
+				Text = lookupManager.ComposeCategoryName(catetory)
+			}).ToList();
+
+            return View(categories);
+        }
+    }
+}
